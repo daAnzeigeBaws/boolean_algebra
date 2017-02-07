e08b0418532be1d66acd8404dca58c4253a7b4d8
Tim Leismann <T.Leismann94@gmail.com>
2017-02-07 14:29:46 +0100

    Menü fertiggestellt, und Anwendungsbeendung eingebaut

--- Boolean_Algebra/Boolean_Algebra/Boolean_Algebra.cpp
+++ Boolean_Algebra/Boolean_Algebra/Boolean_Algebra.cpp
@@ -2,14 +2,39 @@
 //
 
 #include "stdafx.h"
-#include "stdio.h"
+#include "iostream"
 #include "stdlib.h"
+#include "string"
 
 
 int main()
 {
-	printf("Herzlich willkommen zur Prüfung für Boolsche Algebra !\n");
-	system("Pause");
+	std::string input;
+
+	printf("**********************************************************************************************\n"); //Text & Ausgabe des Menüs
+	printf("Herzlich willkommen beim Tool zur Pruefung von Boolscher Algebra!\n\n");
+	printf("Bitte verwenden Sie zur eingabe die folgenden Operatoren:\n");
+	printf("Nicht: \t \t !\n");
+	printf("Und: \t \t &\n");
+	printf("Oder: \t \t |\n");
+	printf("Variablen: \t A,B,C,D\n\n");
+	printf("Zum Verlassen des Programmes tippen sie: exit\n");
+	printf("**********************************************************************************************\n");
+	printf("Bitte geben Sie einen Ausdruck ein: ");
+	std::cin >> input;	
+																						//Eingabe des Audruckes und Schreiben in Variable
+	if (input == "exit")
+	{
+		return 0;
+		system("pause");
+	}
+	else
+	{
+		//Aufruf der Parse-Klasse
+		printf("Else-Pfad aufgerufen\n");
+		system("pause");
+	}
+	
     return 0;
 }
 
