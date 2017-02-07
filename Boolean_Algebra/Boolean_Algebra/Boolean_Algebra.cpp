c82fb16de86ba963c955d72d203846439000a661
Tim Leismann <T.Leismann94@gmail.com>
2017-02-07 11:47:35 +0100

    Test

---
+++ Boolean_Algebra/Boolean_Algebra/Boolean_Algebra.cpp
@@ -0,0 +1,14 @@
+// Boolean_Algebra.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+
+int main()
+{
+	printf("Hello World !");
+	System("Pause");
+    return 0;
+}
+
